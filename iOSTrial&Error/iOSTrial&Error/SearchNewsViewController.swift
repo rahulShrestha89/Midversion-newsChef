91794c54dc8874819fe464b93bb3ba1cca26c3b8
Rahul Shrestha <rahul.shrestha@selu.edu>
2016-09-22 01:26:14 -0500

    foundation for json data to tabbed view

---
+++ iOSTrial&Error/iOSTrial&Error/SearchNewsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchNewsViewController.swift
+//  iOSTrial&Error
+//
+//  Created by Rahul Shrestha on 9/22/16.
+//  Copyright © 2016 Trial&Error. All rights reserved.
+//
+
+import UIKit
+
+class SearchNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
