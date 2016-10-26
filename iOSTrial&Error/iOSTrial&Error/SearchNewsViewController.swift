6103b0d00a8470ba5ffa6fbd752768aca9f394a2
Rahul Shrestha <rahul.shrestha@selu.edu>
2016-10-26 15:30:15 -0500

    perfect list of trending topics from around the world, excludes other languages than english

--- iOSTrial&Error/iOSTrial&Error/SearchNewsViewController.swift
+++ iOSTrial&Error/iOSTrial&Error/SearchNewsViewController.swift
@@ -11,14 +11,22 @@ import TwitterKit
 
 class SearchNewsViewController: UIViewController {
 
+    var TrendingTopicsList: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         getTrendingTopicsFromTwitter()
 
     }
 
     private func getTrendingTopicsFromTwitter(){
     
+        var list :[String] = []
+        
+        let characterset = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLKMNOPQRSTUVWXYZ0123456789# ")
+
+        let hashTag: Character = "#"
+        
         let url =  "\(APIConstants.Twitter.APIBaseURL)1.1/trends/place.json?id=1"
         
         let client = TWTRAPIClient()
@@ -29,6 +37,8 @@ class SearchNewsViewController: UIViewController {
         let request = client.urlRequest(withMethod: "GET", url: statusesShowEndpoint, parameters: params, error: &clientError)
         
         client.sendTwitterRequest(request) { (response, data, connectionError) -> Void in
+            
+            
             if connectionError != nil {
                 print("Error: \(connectionError)")
             }
@@ -39,20 +49,34 @@ class SearchNewsViewController: UIViewController {
                 let jsonResult = json[0] as! NSDictionary
                 
                 let value = jsonResult["trends"] as! NSArray
-                let value1 = value[0]
                 
-                print(value1)
-
-                
+                for item in value{
+                   
+                    let finalValue = item as! NSDictionary
+                    
+                    var trendingName = finalValue["name"]! as! String
+                    
+                    if trendingName.rangeOfCharacter(from: characterset.inverted) == nil {
+                        
+                        if trendingName.characters.contains(hashTag)
+                        {
+                            trendingName.remove(at: trendingName.startIndex)
+                        }
+                        
+                        list.append(trendingName)
+                    }
+                }
+   
             } catch let jsonError as NSError {
                 print("json error: \(jsonError.localizedDescription)")
             }
             
+            self.TrendingTopicsList = list
+            
+            print(self.TrendingTopicsList)
             
         }
-        
-        
-        
+     
     }
 
 }
